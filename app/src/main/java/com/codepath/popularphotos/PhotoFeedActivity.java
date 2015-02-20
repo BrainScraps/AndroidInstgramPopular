2dd382fbae8605706d6472152f9e65a864a674a3
Isaac Elias <isaac@walkerandcobrands.com>
2015-02-20 08:52:53 -0800

    iterating through response written, next is the custom adapter

--- app/src/main/java/com/codepath/popularphotos/PhotoFeedActivity.java
+++ app/src/main/java/com/codepath/popularphotos/PhotoFeedActivity.java
@@ -1,18 +1,71 @@
 package com.codepath.popularphotos;
 
+import android.net.http.AndroidHttpClient;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 
 public class PhotoFeedActivity extends ActionBarActivity {
+
+    public static final String INSTAGRAM_CLIENT_ID = "c395ecb77f1f4def9c8e790b6546df19";
+    private ArrayList<InstagramPhoto> photos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_feed);
         // send request for instagram API
+        photos = new ArrayList<>();
+        fetchPopularPhotos();
+    }
+
+    public void fetchPopularPhotos(){
+        String url = "https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&client_id="
+                + INSTAGRAM_CLIENT_ID ;
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, null, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+               // Expecting JSON object
+               // Log.i("DEBUG", response.toString());
+                JSONArray photosJSON = null;
+                try {
+                    photosJSON = response.getJSONArray("data");
+                    for (int i = 0; i < photosJSON.length(); i++){
+                        JSONObject photoJSON = photosJSON.getJSONObject(i);
+                        InstagramPhoto photo = new InstagramPhoto();
+                        photo.username = photoJSON.getJSONObject("user").getString("username");
+                        photo.caption = photoJSON.getJSONObject("caption").getString("text");
+                        photo.imageUrl = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
+                        photo.imageHeight = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
+                        photo.likeCount = photoJSON.getJSONObject("likes").getInt("count");
+                        photos.add(photo);
+                    }
+
+                } catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+            }
+        });
+
     }
 
 
